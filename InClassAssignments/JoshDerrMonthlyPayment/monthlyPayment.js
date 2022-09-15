35050fa8ee8fb33a2d034d7fafeac4f759ae1ae5
JoshDerr <89755697+JoshDerr@users.noreply.github.com>
2022-09-15 15:39:08 -0500

    Update monthlyPayment.js

--- InClassAssignments/JoshDerrMonthlyPayment/monthlyPayment.js
+++ InClassAssignments/JoshDerrMonthlyPayment/monthlyPayment.js
@@ -2,16 +2,24 @@ var $ = function(id) {
     return document.getElementById(id);
 };
 
-var calculate = function() {
+var click = function() {
     var initialLoanAmount = parseFloat($("initial_loan_amount").value);
     var monthsOfLoan = parseFloat($("months_of_loan").value);
     var annualInterest = parseFloat($("annual_interest").value);
     var monthlyInterestRate = annualInterest / 1200;
     var monthlyPayment;
     
-    monthlyPayment = initialLoanAmount * (monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, monthsOfLoan * -1)));
-        
-    $("monthly_payment").value = "$" + monthlyPayment.toFixed(2);
+    if (initialLoanAmount < 0 || monthsOfLoan < 0 || annualInterest < 0 || annualInterest > 100) {
+        $("monthly_payment").value = "";
+    }
+    else {
+    
+        monthlyPayment = initialLoanAmount * (monthlyInterestRate / (1 - Math.pow(1 + monthlyInterestRate, monthsOfLoan * -1)));
+            
+        $("monthly_payment").value = "$" + monthlyPayment.toFixed(2);
+
+        return false;
+    }
     
 };
 
@@ -19,5 +27,5 @@ window.onload = function(){
     $("initial_loan_amount").value = "";
     $("months_of_loan").value = "";
     $("annual_interest").value = "";
-    $("calculate").onclick = calculate;
+    $("calculate").onclick = click;
 };
