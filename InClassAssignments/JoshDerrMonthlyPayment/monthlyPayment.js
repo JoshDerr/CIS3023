8a0c3739b28bf73b4997ab384879517a604e343e
JoshDerr <89755697+JoshDerr@users.noreply.github.com>
2022-09-15 15:53:28 -0500

    Update monthlyPayment.js

--- InClassAssignments/JoshDerrMonthlyPayment/monthlyPayment.js
+++ InClassAssignments/JoshDerrMonthlyPayment/monthlyPayment.js
@@ -3,13 +3,17 @@ var $ = function(id) {
 };
 
 var click = function() {
+    var fullName = $("full_name").value;
     var initialLoanAmount = parseFloat($("initial_loan_amount").value);
     var monthsOfLoan = parseFloat($("months_of_loan").value);
     var annualInterest = parseFloat($("annual_interest").value);
     var monthlyInterestRate = annualInterest / 1200;
     var monthlyPayment;
     
-    if (initialLoanAmount < 0 || monthsOfLoan < 0 || annualInterest < 0 || annualInterest > 100) {
+    if (fullName == "") {
+        $("monthly_payment").value = "";
+    }
+    else if (initialLoanAmount < 0 || monthsOfLoan < 0 || annualInterest < 0 || annualInterest > 100) {
         $("monthly_payment").value = "";
     }
     else {
